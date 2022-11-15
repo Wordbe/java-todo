a7125a69590ec5b6841bf76887974c76d2cc77ad
Wordbe <seonghojin3@gmail.com>
2022-11-15 18:38:07 +0900

    init

---
+++ src/test/java/co/whitetree/javatodo/JavaTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.whitetree.javatodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
