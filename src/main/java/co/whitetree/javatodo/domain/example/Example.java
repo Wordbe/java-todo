1e170af871191c04999a8fbb3004e768fe19e9fe
Wordbe <seonghojin3@gmail.com>
2023-03-07 19:41:23 +0900

    feat: add equalsandhashcode

--- src/main/java/co/whitetree/javatodo/domain/example/Example.java
+++ src/main/java/co/whitetree/javatodo/domain/example/Example.java
@@ -1,5 +1,7 @@
 package co.whitetree.javatodo.domain.example;
 
+import java.util.Objects;
+
 public class Example {
     public static final String KEY = "E";
 
@@ -18,5 +20,17 @@ public class Example {
         return "Example{" +
                 "id=" + id +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Example example)) return false;
+        return Objects.equals(id, example.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
